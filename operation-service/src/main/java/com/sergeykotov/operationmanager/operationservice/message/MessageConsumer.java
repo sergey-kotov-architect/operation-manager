472138db72e595ba65e2728e25ef0d0ddd90b940
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-24 10:27:13 +0200

    fix MessageConsumer in Operation Service

--- operation-service/src/main/java/com/sergeykotov/operationmanager/operationservice/message/MessageConsumer.java
+++ operation-service/src/main/java/com/sergeykotov/operationmanager/operationservice/message/MessageConsumer.java
@@ -9,9 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class MessageConsumer {
     private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
     private static final ObjectMapper objectMapper = new ObjectMapper();
