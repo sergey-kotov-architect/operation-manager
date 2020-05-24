cfd50b507681b41f88569d6ee6441bed90f3d256
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-25 00:02:37 +0200

    add controllers to Report Service

---
+++ report-service/src/main/java/com/sergeykotov/operationmanager/reportservice/controller/MetricsController.java
@@ -0,0 +1,9 @@
+package com.sergeykotov.operationmanager.reportservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report/metrics")
+public class MetricsController {
+}
---
+++ report-service/src/main/java/com/sergeykotov/operationmanager/reportservice/controller/OpGroupController.java
@@ -0,0 +1,9 @@
+package com.sergeykotov.operationmanager.reportservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report/op_group")
+public class OpGroupController {
+}
