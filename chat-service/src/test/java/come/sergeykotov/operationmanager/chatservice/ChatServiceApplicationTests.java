fc4d14e44a6ee0f44b894af305b4c6f4c6a7d12e
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-25 14:10:02 +0200

    add Chat and Notification Services

---
+++ chat-service/src/test/java/come/sergeykotov/operationmanager/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package come.sergeykotov.operationmanager.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
